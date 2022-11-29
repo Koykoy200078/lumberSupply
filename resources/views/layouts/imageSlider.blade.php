8e37a8c36b2e160f0b0070e312d04f36658625b8
Koykoy200078 <franc200078@gmail.com>
2022-11-29 15:43:41 +0800

    update

--- resources/views/layouts/imageSlider.blade.php
+++ resources/views/layouts/imageSlider.blade.php
@@ -1,14 +1,36 @@
-<div class="slides slowFade">
-        <div class="slide">
-            <img src="{{ URL::asset('assets/img/1.jpg') }}" alt="img"/>
-        </div>
-        <div class="slide">
-            <img src="{{ URL::asset('assets/img/2.jpg') }}" alt="img"/>
-        </div>
-        <div class="slide">
-            <img src="{{ URL::asset('assets/img/3.jpg') }}" alt="img"/>
-        </div>
-        <div class="slide">
-            <img src="{{ URL::asset('assets/img/4.jpg') }}" alt="img"/>
-        </div>
+<div class="CSSgal">
+  <!-- Don't wrap targets in parent -->
+  <s id="s1"></s>
+  <s id="s2"></s>
+  <s id="s3"></s>
+  <s id="s4"></s>
+
+  <div class="slider">
+    <div style="background: #BA8C63">
+      <img src="{{ URL::asset('assets/img/1.jpg') }}" alt="img" />
     </div>
+    <div style="background: #BA8C63">
+      <img src="{{ URL::asset('assets/img/2.jpg') }}" alt="img" />
+    </div>
+    <div style="background: #BA8C63">
+      <img src="{{ URL::asset('assets/img/3.jpg') }}" alt="img" />
+    </div>
+    <div style="background: #BA8C63">
+      <img src="{{ URL::asset('assets/img/4.jpg') }}" alt="img" />
+    </div>
+  </div>
+
+  <div class="prevNext">
+    <div><a href="#s4"><</a><a href="#s2">></a></div>
+    <div><a href="#s1"><</a><a href="#s3">></a></div>
+    <div><a href="#s2"><</a><a href="#s4">></a></div>
+    <div><a href="#s3"><</a><a href="#s1">></a></div>
+  </div>
+
+  <div class="bullets">
+    <a href="#s1">1</a>
+    <a href="#s2">2</a>
+    <a href="#s3">3</a>
+    <a href="#s4">4</a>
+  </div>
+</div>
